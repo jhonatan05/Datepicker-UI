6f6357544a26a9c6a5cd81caabc86f92b1bd46a7
Jhonatan Arevalo <jhon20339@gmail.com>
2025-01-24 16:39:42 -0500

    Frontend: Configurar pagina del calendario

---
+++ Frontend/src/page/CalendarPage.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './CalendarPage.scss'
+import Calendar from '../components/Calendar'
+import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const CalendarPage = () => {
+  return (
+    <section className="calendar-page">
+      <section className="calendar-page__date">
+        <input type="date" />
+      </section>
+      <section className="calendar-page__month">
+        <FontAwesomeIcon icon={faCaretLeft} /> December <FontAwesomeIcon icon={faCaretRight} />
+      </section>
+      <section className="calendar-page__days">
+        <span>S</span>
+        <span>M</span>
+        <span>T</span>
+        <span>W</span>
+        <span>T</span>
+        <span>F</span>
+        <span>S</span>
+        <Calendar />
+      </section>
+      <section className="calendar-page__calendar"></section>
+    </section>
+  )
+}
+
+export default CalendarPage
